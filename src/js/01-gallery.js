aacd1ea21baa851b6a069b0c1bea2991af087af2
DmitriyAtro <atroshchenkod@gmail.com>
2023-05-06 20:30:13 +0300

    Creating repo

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// console.log(galleryItems);
+
+// const galleryContainer = document.querySelector('.gallery');
+
+// function createGaleryItems() {
+//   return galleryItems
+//     .map(
+//       ({ original, preview, description }) => `<li>
+//       <a class="gallery__item" href="${original}">
+// 	<img style="display: block;"
+//     class="gallery__image"
+//     src="${preview}"
+//     alt="${description}" />
+//     </a>
+//     </li>`
+//     )
+//     .join('');
+// }
+// galleryContainer.innerHTML = createGaleryItems(galleryItems);
+
+// new SimpleLightbox('.gallery a', {
+//   captionDelay: '250ms',
+//   captionsData: 'alt',
+// });
