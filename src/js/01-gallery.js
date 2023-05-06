07d37f6fbf3c0168c133a8823d78826e1080100c
DmitriyAtro <atroshchenkod@gmail.com>
2023-05-06 20:45:24 +0300

    Pushing task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,33 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
 
-// import { galleryItems } from './gallery-items.js';
-// // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-// console.log(galleryItems);
+function createGaleryItems() {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) => `<li>
+      <a class="gallery__item" href="${original}">
+	<img style="display: block;"
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}" />
+    </a>
+    </li>`
+    )
+    .join('');
+}
+galleryContainer.innerHTML = createGaleryItems(galleryItems);
 
-// const galleryContainer = document.querySelector('.gallery');
-
-// function createGaleryItems() {
-//   return galleryItems
-//     .map(
-//       ({ original, preview, description }) => `<li>
-//       <a class="gallery__item" href="${original}">
-// 	<img style="display: block;"
-//     class="gallery__image"
-//     src="${preview}"
-//     alt="${description}" />
-//     </a>
-//     </li>`
-//     )
-//     .join('');
-// }
-// galleryContainer.innerHTML = createGaleryItems(galleryItems);
-
-// new SimpleLightbox('.gallery a', {
-//   captionDelay: '250ms',
-//   captionsData: 'alt',
-// });
+new SimpleLightbox('.gallery a', {
+  captionDelay: '250ms',
+  captionsData: 'alt',
+});
