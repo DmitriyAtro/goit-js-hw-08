9fdf08c7e4b17ba48c26489b6988dec0088dd3cc
DmitriyAtro <atroshchenkod@gmail.com>
2023-05-07 21:13:06 +0300

    Task 3 - done.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onInputType, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+const FEEDBACK_FORM = 'feedback-form-state';
+
+let formData = JSON.parse(localStorage.getItem(FEEDBACK_FORM)) || {};
+updateForm();
+function onInputType(evt) {
+  const inputName = evt.target.name;
+  const inputValue = evt.target.value;
+
+  formData[inputName] = inputValue;
+
+  localStorage.setItem(FEEDBACK_FORM, JSON.stringify(formData));
+}
+
+function updateForm() {
+  const savedForm = localStorage.getItem(FEEDBACK_FORM);
+  const parsedForm = JSON.parse(savedForm);
+
+  if (parsedForm) {
+    formEl.elements.message.value = parsedForm.message || '';
+    formEl.elements.email.value = parsedForm.email || '';
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const email = evt.currentTarget.email.value;
+  const message = evt.currentTarget.message.value;
+  const saveFormData = {
+    email,
+    message,
+  };
+
+  console.log(saveFormData);
+  formEl.reset();
+  localStorage.clear();
+  formData = {};
+}
